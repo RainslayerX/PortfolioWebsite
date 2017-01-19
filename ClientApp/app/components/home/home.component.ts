a9b8d846f3474b5a80cae1ac52d1405067c4b29d
Richard Boomsma <richardboomsma@gmail.com>
2017-01-19 03:21:54 +0100

    init

---
+++ ClientApp/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class HomeComponent {
+}
