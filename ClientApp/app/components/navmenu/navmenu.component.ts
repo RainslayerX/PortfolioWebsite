470ffefb1108fb2eea0e70c016ff6161bffe61a9
Richard Boomsma <richardboomsma@gmail.com>
2017-01-22 20:25:31 +0100

    transition added when navigating to home

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'nav-menu',
@@ -6,5 +7,25 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent {
-    loadEnter: boolean = true;
+    loadEnter: boolean = false;
+    loadExit: boolean = false;
+    currentPath: string = '/home';
+
+    constructor(private router: Router) {
+        router.events.subscribe(event => {
+            this.currentPath = this.router.url;
+            if (event instanceof NavigationEnd) {
+                if (this.loadEnter) { //Only activate the exit animation when the enter animation has been fired
+                    this.loadExit = true;
+                    setTimeout(() => this.loadExit = false, 500);
+                }
+                this.loadEnter = false;
+            }
+        })
+    }
+
+    navigateAnimated(targetPath) {
+        this.loadEnter = true;
+        setTimeout(() => this.router.navigate(targetPath), 500);
+    }
 }
